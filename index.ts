def9fbb06458701a35cb20e1c888159ba8f6aecf
Ian Anderson <ian@catalytic.com>
2020-07-22 11:38:23 -0500

    fix: export client type reference
    The default export of the SDK is `typeof CatalyticClient`, which
    limits users to declare a variable as a `CatalyticClient` instance.
    
    For example, the following would produce a TypeScript error prior to
    this update:
    
    ``` typescript
    import CatalyticClient from '@catalytic/sdk';
    
    const client: CatalyticClient = new CatalyticClient();
    ```

--- index.ts
+++ index.ts
@@ -1,6 +1,2 @@
 export * from './src/entities';
-import _CatalyticClient from './src/CatalyticClient';
-
-export const CatalyticClient = _CatalyticClient;
-
-export default CatalyticClient;
+export { default as CatalyticClient } from './src/CatalyticClient';
